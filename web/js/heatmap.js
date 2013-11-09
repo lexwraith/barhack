e7c37505c2c4fc37e8dc48972c2a9600abd0e26a
Helen V. Holmes <helen.v.holmes@gmail.com>
2013-11-09 01:03:18 -0500

    Nope

--- web/js/heatmap.js
+++ web/js/heatmap.js
@@ -1,6 +1,6 @@
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(38, -95),
+    center: new google.maps.LatLng(-15, -55),
     zoom: 4,
     disableDefaultUI: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -33,44 +33,34 @@ function initialize() {
   // Set to map
   map.setOptions({styles: styles});
 
-  // tweets();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'js/tweets.json', true);
+  xhr.onload = function() {
+    loadTweets(this.responseText);
+  };
+  xhr.send();
 
+  function callback(results) {
+    for (var i = 0; i < tweets.length; i++) {
+      var geoJsonObject = tweets.coordinates[i];
+      var geometry = geoJsonObject.geometry;
+    }
+  }
+
+  
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatMapData
+  });
+
+  heatmap.setMap(map);
+}
+
+
+function loadScript() {
   var script = document.createElement("script");
   script.type = "text/javascript";
   script.src = "http://maps.googleapis.com/maps/api/js?libraries=geometry,visualization&key=AIzaSyD8o5eliFAEzQ4uHwjOUQMGe7wNCbZGTeE&sensor=true&callback=initialize";
   document.body.appendChild(script);
-
-  // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        script.src = 'http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week';
-        document.getElementsByTagName('head')[0].appendChild(script);
-
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      function eqfeed_callback(results){
-        for (var i = 0; i < results.features.length; i++) {
-          var earthquake = results.features[i];
-          var coords = earthquake.geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
 }
 
-// var script = 'js/tweets.json';
-
-// var tweets = $.getJSON(script, function(data) {
-//     var tweetly = script.tweets[i];
-//     var coords = tweets.coordinates;
-//     console.log(coords);
-//     var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//     var marker = new google.maps.Marker({
-//       position: latLng,
-//       map: map
-//     });
-//   });
-
-window.onload = initialize;
+window.onload = loadScript;
